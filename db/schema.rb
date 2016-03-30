0b804984cc84ce1171f1a937de3b6508ac974a74
elfedy <federodriguez.88@gmail.com>
2016-03-30 17:51:18 -0300

    Created Category and Product models and ran the db migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330194511) do
+ActiveRecord::Schema.define(version: 20160330200253) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer  "price"
     t.string   "user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id"
+    t.string   "description"
+    t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
