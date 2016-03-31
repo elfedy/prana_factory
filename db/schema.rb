0a860c237c5ef1121e88ebf1b56abd10e6d02a21
elfedy <federodriguez.88@gmail.com>
2016-03-31 09:46:10 -0300

    Added description column to categories and ran the migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160330200253) do
+ActiveRecord::Schema.define(version: 20160331124208) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "orders", force: :cascade do |t|
