c98e9d63d288c8c560ad7e64bb48db3df1e42cf6
elfedy <federodriguez.88@gmail.com>
2016-04-11 14:10:41 -0300

    Products nested into categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ActiveRecord::Base
   validates :name, presence: true
   has_many :products, :dependent => :destroy
+
+  accept_nested_attributes_for => :products
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'comprar'                 => 'static_pages#comprar'
 
   resources :categories
-  resources :products
+  resources :products , except: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
