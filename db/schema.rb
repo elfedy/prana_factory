7122b6b2908aa2edad477ada34270d80bf497677
elfedy <federodriguez.88@gmail.com>
2016-04-13 19:51:50 -0300

    Generated model for sku and ran pending migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160331193316) do
+ActiveRecord::Schema.define(version: 20160413225057) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -27,11 +27,27 @@ ActiveRecord::Schema.define(version: 20160331193316) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "packages", force: :cascade do |t|
+    t.integer  "category_id"
+    t.integer  "units"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "category_id"
     t.string   "description"
     t.string   "image_url"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "skus", force: :cascade do |t|
+    t.integer  "category_id"
+    t.integer  "product_id"
+    t.integer  "package_id"
+    t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
