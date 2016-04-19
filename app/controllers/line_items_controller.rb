50c4c029b945767f7dc16ed57eefa0de2e7d6423
elfedy <federodriguez.88@gmail.com>
2016-04-19 14:44:09 -0300

    added line_item create action (untested)

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,9 @@
 class LineItemsController < ApplicationController
   def create
+    @sku = Sku.find(params[:sku_id])
+    @order = current_order
+    @order.line_items.create(price: @sku.price, sku_id: @sku.id)
+    @order.price += @sku.price
   end
 
   def destroy
