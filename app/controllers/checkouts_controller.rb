f52eab5c98e36f9acc4da7f487245300d42d7bed
elfedy <federodriguez.88@gmail.com>
2016-04-26 12:25:50 -0300

    Checkout create action

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,8 +1,19 @@
 class CheckoutsController < ApplicationController
   def new
+    @checkout = Checkout.new
+    @order = current_order
   end
 
   def create
+    @checkout = Checkout.new(params[:checkout])
+    @checkout.add_line_items_from_order(current_order)
+
+    if @checkout.save
+      Order.destroy(session[:order_id])
+      session[:order_id] = nil
+      redirect_to comprar_path
+    end
+
   end
 
   def destroy
--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,3 +1,12 @@
 class Checkout < ActiveRecord::Base
-  has_many :line_items
+  has_many :line_items, :dependent => :destroy
+
+  DELIVERY_TIME_OPTIONS = [ "9:00 a 12:00", "12:00 a 15:00", "15:00 a 18:00", "18:00 a 21:00"]
+
+  def add_line_items_from_order(order)
+    order.line_items.each do |item|
+      item.order_id = nil
+      line_items << item
+    end
+  end
 end
