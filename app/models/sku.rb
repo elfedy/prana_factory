2d54576f430b9f0d3f9f222b4198203e57feafa7
elfedy <federodriguez.88@gmail.com>
2016-05-13 10:48:09 -0300

    Added presence validations with product_id and package_id for sku model

--- app/models/sku.rb
+++ app/models/sku.rb
@@ -1,6 +1,7 @@
 class Sku < ActiveRecord::Base
   default_scope { order(product_id: :asc) }
 
+  validates :product_id, :package_id, presence: true
   validates_associated :products, :packages
 
   belongs_to :category
