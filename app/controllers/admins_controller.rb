5bba52d8c57613e84045c7830530f0a59be7af79
elfedy <federodriguez.88@gmail.com>
2016-05-17 16:31:52 -0300

    add admin index new and destroy actions

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,21 @@
 class AdminsController < ApplicationController
   def index
+    @admins = Admin.all
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def create
+    
+  end
+
+  def destroy
+    @admin = Admin.find(params[:id])
+    if @admin.destroy
+      redirect_to admins_path
+    end
   end
 
   def actions
