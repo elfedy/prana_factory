4400432d6f6baccae00c989ea3ede8bbcc509caf
elfedy <federodriguez.88@gmail.com>
2016-05-27 11:06:51 -0300

    Refactor nuestros_productos view using partials for categories and products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,11 @@
+#Tecnically they are not static pages so controller must be renamed to "User Pages" or similar
+
 class StaticPagesController < ApplicationController
   def inicio
   end
 
   def nuestros_productos
+    @categories = Category.all
   end
 
   def comprar
