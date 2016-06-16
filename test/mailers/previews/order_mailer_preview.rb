06f85c4ae6bdc2d7b909c42bd679d3501759d542
elfedy <federodriguez.88@gmail.com>
2016-06-15 22:48:02 -0300

    mail and responsive styling to get the page ready for productio

--- test/mailers/previews/order_mailer_preview.rb
+++ test/mailers/previews/order_mailer_preview.rb
@@ -3,12 +3,14 @@ class OrderMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/order_confirmation_user
   def order_confirmation_user
-    OrderMailer.order_confirmation_user
+    checkout = Checkout.first
+    OrderMailer.order_confirmation_user(checkout)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/order_mailer/order_confirmation_admin
   def order_confirmation_admin
-    OrderMailer.order_confirmation_admin
+    checkout = Checkout.first
+    OrderMailer.order_confirmation_admin(checkout)
   end
 
 end
