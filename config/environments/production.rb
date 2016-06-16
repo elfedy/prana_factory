e82db65e6dcf80e792f67f8af957dc4a4154154a
elfedy <federodriguez.88@gmail.com>
2016-06-16 15:16:56 -0300

    changed production image host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.action_mailer.asset_host = "pranafactory.herokuapp.com"
-  config.action_mailer.default_url_options = { host: "localhost:300"}
+  config.action_mailer.asset_host = "www.pranafactory.com"
+  config.action_mailer.default_url_options = { host: "www.pranafactory.com"}
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
